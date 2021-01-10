988e6231ddf272c0f3700c819efa1627a3d95995
udit2910 <uditdj29@gmail.com>
2021-01-10 15:51:12 +0530

    server changes

--- server.js
+++ server.js
@@ -2,17 +2,16 @@ const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
 const path = require('path');
-const PORT = process.env.port || 5000;
+
+connectDB();
 
 app.use(express.json({ extended: false }));
 
 app.use('/api/users', require('./routes/users'));
-app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
-connectDB();
-
-if (process.env === 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) =>
@@ -20,4 +19,6 @@ if (process.env === 'production') {
   );
 }
 
-app.listen(PORT, () => console.log(`server running on ${PORT}`));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
