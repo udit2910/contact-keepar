20c2b0951324025a19bc3e0dfdc4a9b708eea244
udit2910 <uditdj29@gmail.com>
2021-01-10 15:37:26 +0530

    chore : prod deployment

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
-
+const path = require('path');
 const PORT = process.env.port || 5000;
-
-connectDB();
 
 app.use(express.json({ extended: false }));
 
@@ -12,6 +10,14 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
 
-app.get('/', (req, res) => res.json({ msg: 'Hello world' }));
+connectDB();
+
+if (process.env === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 app.listen(PORT, () => console.log(`server running on ${PORT}`));
